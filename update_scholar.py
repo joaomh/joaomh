54a3b432b02e3f6ede7d61bfd66c54fedcae4c8f
João Manoel Herrera Pinheiro <joaomh@protonmail.com>
2025-09-03 18:20:04 -0300

    Update update_scholar.py

--- update_scholar.py
+++ update_scholar.py
@@ -1,38 +1,38 @@
-from scholarly import scholarly
-# Author info
-scholar_id = 'bXATl38AAAAJ' 
-author = scholarly.search_author_id(scholar_id)
-author = scholarly.fill(author, sections=['publications'])
+# from scholarly import scholarly
+# # Author info
+# scholar_id = 'bXATl38AAAAJ' 
+# author = scholarly.search_author_id(scholar_id)
+# author = scholarly.fill(author, sections=['publications'])
 
-# Order by data
-publications = sorted(
-    author['publications'], 
-    key=lambda x: x.get('bib', {}).get('pub_year', 0), 
-    reverse=True
-)
+# # Order by data
+# publications = sorted(
+#     author['publications'], 
+#     key=lambda x: x.get('bib', {}).get('pub_year', 0), 
+#     reverse=True
+# )
 
-# 5 recents
-publications = publications[:5]
+# # 5 recents
+# publications = publications[:5]
 
-# Markdown list
-pub_list = "\n".join([
-    f"- [{pub['bib']['title']}]({pub.get('pub_url', 'https://scholar.google.com')})"
-    for pub in publications
-])
+# # Markdown list
+# pub_list = "\n".join([
+#     f"- [{pub['bib']['title']}]({pub.get('pub_url', 'https://scholar.google.com')})"
+#     for pub in publications
+# ])
 
-# read readme
-with open("README.md", "r") as f:
-    readme = f.read()
+# # read readme
+# with open("README.md", "r") as f:
+#     readme = f.read()
 
-# find the place to replace
-marker_start = "<!--SCHOLAR-START-->"
-marker_end = "<!--SCHOLAR-END-->"
+# # find the place to replace
+# marker_start = "<!--SCHOLAR-START-->"
+# marker_end = "<!--SCHOLAR-END-->"
 
-# replace
-before = readme.split(marker_start)[0] + marker_start + "\n"
-after = "\n" + marker_end + readme.split(marker_end)[1]
-new_readme = before + pub_list + after
+# # replace
+# before = readme.split(marker_start)[0] + marker_start + "\n"
+# after = "\n" + marker_end + readme.split(marker_end)[1]
+# new_readme = before + pub_list + after
 
-# write on readme
-with open("README.md", "w") as f:
-    f.write(new_readme)
+# # write on readme
+# with open("README.md", "w") as f:
+#     f.write(new_readme)
